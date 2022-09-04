76e90f9fed2724512d8422622e89157f0378d21b
Rafał <raf.zielen@gmail.com>
2022-09-04 15:03:02 +0200

    trudne początki

--- ping-pong.js
+++ ping-pong.js
@@ -1,1 +1,27 @@
-const cavas = document.getElementById('gameCanvas');
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "white";
+
+function drawPaddle(x, y) {
+    ctx.fillRect(x, y, 20, 100);
+}
+drawPaddle(770, 100);
+drawPaddle(10, 300);    
+
+ctx.fillText("Ping Pong", 370, 50);
+
+function drawText(text, x, y) {
+    ctx.fillText(text, x, y);
+};
+
+drawText("3", 300,50);
+drawText("6", 500, 50);
+
+function drawCircle(x,y,r) {
+    ctx.BeginPath();
+    ctx.arc(x,y,r,0, Math.PI * 2, true);
+    ctx.closePath();
+    
+}
+
+drawCircle(400, 250, 15);
